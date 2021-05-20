cf3ba37a59642fc336eadbb88416cfbc55f47879
Rodrigo Sousa <rodrigudesousa@gmail.com>
2021-05-20 16:24:46 -0400

    projeto criado

---
+++ src/main/java/com/rodrigo/doador/DoadorApplication.java
@@ -0,0 +1,13 @@
+package com.rodrigo.doador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoadorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rodrigo/doador/DoadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigo.doador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
