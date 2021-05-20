1b2f231e4e80ffb3b427d80b34166e8fb8813d2c
Rodrigo Sousa <rodrigudesousa@gmail.com>
2021-05-20 16:41:15 -0400

    REST testado

---
+++ src/main/java/com/rodrigo/doador/resources/PessoaResource.java
@@ -0,0 +1,15 @@
+package com.rodrigo.doador.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/pessoas")
+public class PessoaResource {
+
+	@RequestMapping(method=RequestMethod.GET)
+	public String listar() {
+		return "rest funcionando";
+	}
+}
